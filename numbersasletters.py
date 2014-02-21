b79e9abf32036257f61fdd2ddbc418d1799c0052
sfader <sfader@gmail.com>
2014-02-21 13:36:04 -0500

    loops set up but answer is incorrect

--- numbersasletters.py
+++ numbersasletters.py
@@ -5,28 +5,55 @@ teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen"
 tens = ["twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"]
 hundreds = ["onehundred", "twohundred", "threehundred", "fourhundred", "fivehundred", "sixhundred", "sevenhundred", "eighthundred", "ninehundred"]
 thousands = ["onethousand"]
-ands = "and"
 onesSum = 0.0
 teensSum = 0.0
 tensSum = 0.0
 hundredsSum = 0.0
 thousandsSum = 0.0
+totalSum = 0.0
 
   
 #count ones place
 for i in range (0,len(ones)):
   onesSum = onesSum +len(ones[i])
-print onesSum
+#print onesSum
 
 #count teens
-for j in range (0,len(teens)):
+for i in range (0,len(teens)):
   teensSum = teensSum + len(teens[i])
-print teensSum
+#print teensSum
 
 #count tens place
+for i in range (0,len(tens)):
+  for j in range (0, len(ones)):
+    tensSum = tensSum + len(tens[i]) + len(ones[j])
+#print tensSum
 
-for k in range (0,len(tens)):
-  for l in range (0, len(ones)):
-    tensSum = tensSum + len(tens[k]) + len(ones[l])
-print tensSum
+#count hundreds place up to 20 eg. (101, 102, 102,... 201,202,203,....)
+for i in range (0,len(hundreds)):
+  for j in range (0,len(ones)):
+      hundredsSum = hundredsSum + len(hundreds[i]) + len("and") + len(ones[j])
+      print hundreds[i] + "and" + ones[j]
+for i in range (0,len(hundreds)):
+  for j in range (0,len(teens)):
+      hundredsSum = hundredsSum + len(hundreds[i]) + len("and") + len(teens[j])
+      #print hundreds[i] + "and" + teens[j]
+
+#count hundreds place from 20 - 99 eg. (121, 122.....221,222....)
+for i in range (0,len(hundreds)):
+  for j in range(0, len(tens)):
+    hundredsSum = hundredsSum + len(hundreds[i]) + len("and") + len(tens[j])
+    #print str(hundreds[i]) + str(tens[j])
+    for k in range(0,len(ones)):
+       hundredsSum = hundredsSum + len(hundreds[i]) + len("and") + len(tens[j]) +len(ones[k])
+      # print str(hundreds[i]) + str(tens[j]) + str(ones[k])
+
+#count thousands place
+for i in range (0,len(thousands)):
+  thousandsSum = thousandsSum + len(thousands[i])
+
+totalSum = thousandsSum + hundredsSum + tensSum + teensSum + onesSum
+ 
+print totalSum
+
 
