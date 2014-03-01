0d63c6aed4c376235ad50394cbf47a869f879864
sfader <sfader@gmail.com>
2014-03-01 12:05:48 -0500

    tried to make it faster, made it slower

--- fifthpowerdigits.py
+++ fifthpowerdigits.py
@@ -1,24 +1,15 @@
-# sum of fifth power digits
-
-
+# Sophie Fader project euler 30
 # any number that can be written as the sum of its digits to the 5th power
-
-# for loop through numbers, hold current number, make a list of current number digits, raise each digit to the 5th power, test equivalence
-# make a list of successes 
 
 succ = []
 curr =0
 total = 0
 
-
 for i in range (2, 531441):
   curr = str(i)
   digsum = 0
   for k in range(0, len(curr)):
-      digsum = digsum + pow(int(curr[k]),5)
+      digsum = digsum + (int(curr[k])**5)
   if i==digsum:
-    succ.append(digsum)
-for j in range (0, len(succ)):
-  total = total + succ[j]
-
+    total = total + digsum
 print total
