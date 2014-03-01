61deb03e3cf4d403d3538f1b0340c714b5529d47
sfader <sfader@gmail.com>
2014-03-01 11:31:18 -0500

    sketched idea

---
+++ fifthpowerdigits.py
@@ -0,0 +1,8 @@
+# sum of fifth power digits
+
+
+# five digit numers 10000 >= x <= 99999
+
+# for loop through numbers with five digits, hold current number, make a list of current number digits, raise each digit to the 5th power, test equivalence
+# make a list of successes 
+
