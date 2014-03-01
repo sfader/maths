905810ba11e297a3907ac1c7fe78c409177a46e8
sfader <sfader@gmail.com>
2014-03-01 11:40:57 -0500

    testing theory

--- fifthpowerdigits.py
+++ fifthpowerdigits.py
@@ -6,3 +6,13 @@
 # for loop through numbers with five digits, hold current number, make a list of current number digits, raise each digit to the 5th power, test equivalence
 # make a list of successes 
 
+succ = []
+curr =0
+
+for i in range (10000, 10500):
+  curr = str(i)
+  digsum = pow(int(curr[0]),5) + pow(int(curr[1]),5) + pow(int(curr[2]),5) + pow(int(curr[3]),5) + pow(int(curr[4]),5)
+  #print i, digsum
+  if i==digsum:
+    succ.append(digsum)
+print succ
